86b3f29289360f05c10ac2fb1bc4d7bfffd0c637
Lakshay2322306 <156868259+Lakshay2322306@users.noreply.github.com>
2024-10-27 18:57:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
+# main_bot.py
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from src.utils.pairing import find_partner
-from src.utils.user_info import update_user_info
+from telegram.ext import Updater, CommandHandler, CallbackContext
 import os
 
-MAIN_BOT_TOKEN = os.getenv("MAIN_BOT_TOKEN")
-
-updater = Updater(MAIN_BOT_TOKEN)
-
-# Command to start the bot
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Welcome to the Anonymous Chat Bot! Type /connect to start chatting with a random user.")
 
-# Connect command to pair users
 def connect(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
     partner_id = find_partner(user_id)
-
-    # Send user info to info_bot for storage
     update_user_info(os.getenv("INFO_BOT_TOKEN"), user_id, update.message.from_user)
 
     if partner_id:
@@ -26,14 +17,9 @@ def connect(update: Update, context: CallbackContext):
     else:
         update.message.reply_text("Waiting for a user to connect...")
 
-# Command to disconnect
-def disconnect(update: Update, context: CallbackContext):
-    user_id = update.message.from_user.id
-    context.bot.send_message(chat_id=user_id, text="You have disconnected.")
-
-updater.dispatcher.add_handler(CommandHandler("start", start))
-updater.dispatcher.add_handler(CommandHandler("connect", connect))
-updater.dispatcher.add_handler(CommandHandler("disconnect", disconnect))
-
-updater.start_polling()
-updater.idle()
+def run_main_bot():
+    updater = Updater(os.getenv("MAIN_BOT_TOKEN"))
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.dispatcher.add_handler(CommandHandler("connect", connect))
+    updater.start_polling()
+    updater.idle()
